3fd3cc32be37f0cd59d86e024c6aa27d60203f9f
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-09-06 12:02:41 -0400

    Facebook and Google ajax handlers, NOT Completed

--- ajax/ajax-login.php
+++ ajax/ajax-login.php
@@ -1,7 +1,14 @@
 <?php
 add_action('wp_ajax_marcador_logout', 'marcador_logout_callback');
+
 add_action('wp_ajax_nopriv_marcador_login', 'marcador_login_callback');
 add_action('wp_ajax_nopriv_marcador_register', 'marcador_register_callback');
+
+add_action('wp_ajax_nopriv_marcador_google_login', 'marcador_login_callback');
+add_action('wp_ajax_nopriv_marcador_google_register', 'marcador_register_callback');
+
+/*add_action('wp_ajax_nopriv_marcador_facebook_login', 'marcador_login_callback');
+add_action('wp_ajax_nopriv_marcador_facebook_register', 'marcador_register_callback');*/
 
 /**
  * Logout Ajax callback
@@ -23,16 +30,67 @@ function marcador_logout_callback() {
 function marcador_login_callback() {
   if ( !valid_login_post_fields() ) send_error_response("All fields required");
 
-  $data                 = new stdClass;
-  $data->user_login     = $_POST['username'];
-  $data->user_password  = $_POST['password'];
-  $data->remember       = FALSE;
-  if ( !valid_credentials($data) ) send_error_response("Invalid credentials");
+  if ( !isset($_POST['auth']) ) $data = marcador_login();
+  else if ( isset($_POST['auth_type']) && $_POST['auth_type'] === "google" )
+    $data = marcador_google_login();
+  else if ( isset($_POST['auth_type']) && $_POST['auth_type'] === "facebook" )
+    $data = marcador_facebook_login();
+  else send_error_response("Invalid credentials");
 
   $body         = new stdClass;
   $body->data   = "Hello " . $data->user_login . "!";
   $body->valid  = TRUE;
   send_response( json_encode($body) );
+}
+
+function marcador_login() {
+  $data                 = new stdClass;
+  $data->user_login     = $_POST['username'];
+  $data->user_password  = $_POST['password'];
+  $data->remember       = FALSE;
+  if ( !valid_credentials($data) ) send_error_response("Invalid credentials");
+  return $data;
+}
+
+function marcador_google_login() {
+  $data                 = new stdClass;
+  $data->user_login     = $_POST['username'];
+
+  $user_id =  username_exists( $data->user_login ) || 
+              email_exists( $data->user_login );
+  if ( !$user_id ) send_error_response("Invalid credentials");
+
+  $id_token = $_POST['auth'];
+  $url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" . 
+            $id_token;
+  $response = wp_remote_get( $url );
+  if ( is_wp_error($response) ) send_error_response("Couldn't validate");
+
+  $body = json_decode( $reponse['body'] );
+  if ($body->email_verified === true && $body->email === $data->user_login)
+    return $data;
+
+  send_error_response("Couldn't validate");
+}
+
+function marcador_facebook_login() {
+  $data                 = new stdClass;
+  $data->user_login     = $_POST['username'];
+
+  $user_id =  username_exists( $data->user_login ) || 
+              email_exists( $data->user_login );
+  if ( !$user_id ) send_error_response("Invalid credentials");
+
+  $id_token = $_POST['auth'];
+  $url = "https://graph.facebook.com/debug_token?input_token={$id_token}&access_token={$id_token}";
+  $response = wp_remote_get( $url );
+  if ( is_wp_error($response) ) send_error_response("Couldn't validate");
+
+  $body = json_decode( $reponse['body'] );
+  if ($body->email_verified === true && $body->email === $data->user_login)
+    return $data;
+
+  send_error_response("Couldn't validate");
 }
 
 
